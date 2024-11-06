363cf7f7189ee20760f24fc03d83c5f26205dd18
ksngh <chzhqk98@naver.com>
2024-11-07 00:11:11 +0900

    chore : set up initial project files

---
+++ src/main/java/com/delivery_project/DeliveryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.delivery_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/delivery_project/DeliveryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
