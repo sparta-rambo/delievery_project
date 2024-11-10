5098c7a12a03336dd9c8b2bb248ba102e7c8df53
kgrace2366 <kgrace2366@naver.com>
2024-11-10 21:01:58 +0900

    회원기능 구현

--- src/main/java/com/delivery_project/entity/User.java
+++ src/main/java/com/delivery_project/entity/User.java
@@ -2,19 +2,23 @@ package com.delivery_project.entity;
 
 import com.delivery_project.enums.UserRoleEnum;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+
+import java.util.UUID;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
 @Table(name = "p_users")
-public class User {
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class User extends Timestamped{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private UUID id;
 
     @Column(nullable = false, unique = true, length = 50)
     private String username;
