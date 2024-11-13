abfb0c58b065902a447cf1020b0da96c9f2d7a2d
ksngh <chzhqk98@naver.com>
2024-11-13 23:48:12 +0900

    feat : Order 생성에서 validation 추가 (#38)

--- src/main/java/com/delivery_project/controller/api/OrderRestController.java
+++ src/main/java/com/delivery_project/controller/api/OrderRestController.java
@@ -5,13 +5,13 @@ import com.delivery_project.dto.response.MessageResponseDto;
 import com.delivery_project.entity.User;
 import com.delivery_project.enums.SuccessMessage;
 import com.delivery_project.service.OrderService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,7 +21,7 @@ public class OrderRestController {
     private final OrderService orderService;
 
     @PostMapping()
-    public ResponseEntity<?> createOrder(@RequestBody OrderRequestDto.Create orderRequestDto) {
+    public ResponseEntity<?> createOrder(@Valid @RequestBody OrderRequestDto.Create orderRequestDto) {
         //security
         User user = new User();
 
--- src/main/java/com/delivery_project/dto/request/OrderRequestDto.java
+++ src/main/java/com/delivery_project/dto/request/OrderRequestDto.java
@@ -1,5 +1,6 @@
 package com.delivery_project.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,10 +17,18 @@ public class OrderRequestDto {
     @NoArgsConstructor
     public static class Create {
 
+        @NotNull(message = "주문 항목이 존재하지 않습니다.")
         private List<OrderItemRequestDto.Create> orderItemRequestDtos;
+
+        @NotNull(message = "식당 id가 존재하지 않습니다.")
         private UUID restaurantId;
+
+        @NotNull(message = "주문 방식이 존재하지 않습니다.")
         private String orderType;
+
+        @NotNull(message = "배송지가 존재하지 않습니다.")
         private String deliveryAddress;
+
         private String deliveryRequest;
 
         public Create(List<OrderItemRequestDto.Create> orderItemRequestDtos, UUID restaurantId, String orderType, String deliveryAddress, String deliveryRequest) {
