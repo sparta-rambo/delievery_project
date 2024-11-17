132a206ff6dbad18a0e6666d6d8a47ee4493a06f
yewonkim301 <yeowne301@gmail.com>
2024-11-17 14:30:47 +0900

    refactor : Restaurant 카테고리별 조회 관련 검색 로직 추가(#SPA-2, 15)

--- src/main/java/com/delivery_project/service/RestaurantService.java
+++ src/main/java/com/delivery_project/service/RestaurantService.java
@@ -157,9 +157,16 @@ public class RestaurantService {
     }
 
     @Transactional(readOnly = true)
-    public Page<RestaurantResponseDto> getRestaurantsByCategory(PageRequest pageRequest, UUID categoryId) {
+    public Page<RestaurantResponseDto> getRestaurantsByCategory(PageRequest pageRequest, UUID categoryId, String search) {
         // Querydsl로 조건을 생성 (카테고리에 해당하는 가게 조회)
         BooleanExpression predicate = qRestaurant.category.id.eq(categoryId);
+
+        predicate = predicate.and(qRestaurant.isHidden.isFalse());
+
+        // 검색 조건이 있는 경우
+        if (search != null && !search.isEmpty()) {
+            predicate = predicate.and(qRestaurant.name.containsIgnoreCase(search));
+        }
 
         // Querydsl 페이징 적용
         List<Restaurant> restaurants = restaurantRepository.findRestaurants(predicate, pageRequest);
