8986793f80384d6e9927c67fec4eb231e8bd40bc
ksngh <chzhqk98@naver.com>
2024-11-18 11:33:43 +0900

    fix : WebSecurityConfig,test entity

--- src/main/java/com/delivery_project/repository/implement/RestaurantRepositoryCustom.java
+++ src/main/java/com/delivery_project/repository/implement/RestaurantRepositoryCustom.java
@@ -1,10 +1,8 @@
 package com.delivery_project.repository.implement;
 
 import com.delivery_project.entity.Restaurant;
-import com.delivery_project.entity.TestEntity;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Repository;
